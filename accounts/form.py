080df945198dd65bb6043436a369f49a06d06dc0
Sterling Savariau <sterlingsavariau@Sterlings-MacBook-Pro-2.local>
2021-12-31 22:30:11 -0500

    Added First Name & Last Name Requirement To Signup Form

--- accounts/form.py
+++ accounts/form.py
@@ -1,10 +1,21 @@
-from allauth.account.forms import SignupForm
 from django import forms
- 
- 
-class CustomSignupForm(SignupForm):
-    first_name = forms.CharField(max_length=30, label='First Name')
-    last_name = forms.CharField(max_length=30, label='Last Name')
+
+class CustomSignupForm(forms.Form):
+    first_name = forms.CharField(
+        max_length=30, 
+        label='First Name',
+        widget=forms.TextInput(
+            attrs={"placeholder": ("First name"), "autocomplete": "First name"}
+        ),
+    )
+
+    last_name = forms.CharField(
+        max_length=30, 
+        label='Last Name',
+        widget=forms.TextInput(
+            attrs={"placeholder": ("Last name"), "autocomplete": "Last name"}
+        ),
+        )
  
     def save(self, request):
         user = super(CustomSignupForm, self).save(request)
