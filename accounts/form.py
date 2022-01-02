cd6f0f28aec21eb6594ebcc62354dbe817e5f41e
Sterling Savariau <sterlingsavariau@Sterlings-MacBook-Pro-2.local>
2022-01-02 03:57:34 -0500

    Fix 'super object has no attribute save' Error

--- accounts/form.py
+++ accounts/form.py
@@ -21,5 +21,5 @@ class CustomSignupForm(forms.Form):
         user = super(CustomSignupForm, self).save(request)
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
-        user.save()
+        user().save()
         return user
